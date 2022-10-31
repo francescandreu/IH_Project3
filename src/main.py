e26fa3197f16c0d8e47186c4fd61a96c353f1f76
Francesc Andreu <francesc.andreumacias@gmail.com>
2022-10-31 09:51:22 +0100

    Testing functionalities

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import os
+import requests
+import json
+from dotenv import load_dotenv
+import pandas as pd
+import geopandas as gpd
+from cartoframes.viz import Map, Layer, popup_element
+
+
+def main():
+
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
